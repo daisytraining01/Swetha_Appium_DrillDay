2e3c2ae427e474baf77925bf2e210fc6d407ae50
daisytraining01 <64245212+daisytraining01@users.noreply.github.com>
2020-05-27 21:07:19 +0530

    Add files via upload
    code add

---
+++ src/test/java/com/appium/pageObjects/pageObjects.java
@@ -0,0 +1,59 @@
+package com.appium.pageObjects;
+
+import org.openqa.selenium.By;
+
+
+
+public class pageObjects  {
+	
+	
+ public static final	By userName = By.id("com.experitest.ExperiBank:id/usernameTextField");
+ public static final	By password = By.id("com.experitest.ExperiBank:id/passwordTextField");
+ public static final	By loginButton = By.id("com.experitest.ExperiBank:id/loginButton");
+ 
+ 
+ 
+ public static final	By makePaymentButton = By.id("com.experitest.ExperiBank:id/makePaymentButton");
+ 
+ 
+ public static final	By phoneTextField = By.id("com.experitest.ExperiBank:id/phoneTextField");
+ public static final	By nameTextField = By.id("com.experitest.ExperiBank:id/nameTextField");
+ public static final	By selectName = By.id("android:id/text1");
+ public static final	By country = By.id("com.experitest.ExperiBank:id/countryButton");
+ public static final	By India = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ListView/android.widget.TextView[1]");
+ public static final	By sendPaymentButton = By.id("com.experitest.ExperiBank:id/sendPaymentButton");
+ 
+ 
+ public static final	By confirmationButton = By.id("android:id/button1");
+ 
+ 
+ public static final	By Logout = By.id("com.experitest.ExperiBank:id/logoutButton");
+ 
+
+ 
+ 
+ 
+ //------------KEVIN-------------------
+ public static final	By amount = By.id("com.avrapps.emicalculator:id/amount");
+ public static final	By interest = By.id("com.avrapps.emicalculator:id/interest");
+ public static final	By calculate = By.id("com.avrapps.emicalculator:id/button");
+ public static final	By save = By.id("com.avrapps.emicalculator:id/button2");
+ public static final	By emi_Name = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v7.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.EditText");
+ public static final	By ok_button = By.id("android:id/button1");
+ 
+
+ public static final	By confirmationButton1 = By.id("android:id/button3");
+ 
+ public static final	By search1 = By.id("android:id/search_src_text");
+ 
+ public static final	By search2 = By.id("patel.krupesh.amazinguniversefactsapp:id/title_lv_item");
+
+ public static final	By search3 = By.id("android:id/search_src_text");
+
+ public static final	By titan2 = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.TextView[2]");
+
+ public static final	By search4 = By.id("patel.krupesh.amazinguniversefactsapp:id/message");
+
+ public static final	By search5 = By.id("patel.krupesh.amazinguniversefactsapp:id/title_content");
+
+}
---
+++ src/test/java/com/appium/test/SwethaTasty.java
@@ -0,0 +1,130 @@
+package com.appium.test;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.appium.pageObjects.pageObjects_Swetha;
+import com.helper.DatabaseConnection;
+import com.helper.ExcelUtil;
+import com.helper.UserActions;
+public class SwethaTasty {
+	UserActions user;
+   AndroidDriver driver;
+   private String accessKey = null;
+
+@Parameters({"Device"})		
+  @BeforeTest
+	public void setUp( String Device) throws MalformedURLException {
+	  DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	  if (Device.contentEquals("Emulator_Device")) {
+	   System.out.println("Entering Emulator device");
+	   desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("platformVersion", "9");
+		desiredCapabilities.setCapability("udid", "emulator-5554");
+		desiredCapabilities.setCapability("deviceName", "Emulator");
+	    desiredCapabilities.setCapability("deviceName", "Android SDK built for x86");
+	    desiredCapabilities.setCapability("appPackage", "com.buzzfeed.tasty");
+	    desiredCapabilities.setCapability("appActivity", "com.buzzfeed.tasty.LauncherActivity");
+	    URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+	    driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+	    user= new UserActions(driver);
+	  }
+	   
+	   else if (Device.contentEquals("Cloud_Device")) {
+		   System.out.println("Entering cloud");
+		   accessKey = "eyJ4cC51Ijo0NTU2OTcsInhwLnAiOjQ1NTY4OSwieHAubSI6Ik1UVTRPVE0yTnprMU1EQXlNdyIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE5MDQ3Mjc5NTAsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.9PEKvslTXNVQjibm_oLIBni8iknsIwo1etQfjBMQ8ME";
+		   
+				System.out.println("Entering Cloud device");
+				desiredCapabilities.setCapability("testName", "Quick Start Android Native Demo");
+				desiredCapabilities.setCapability("accessKey", accessKey);
+				desiredCapabilities.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
+				desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.buzzfeed.tasty");
+				desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.buzzfeed.tasty.LauncherActivity");
+		        driver = new AndroidDriver<>(new URL("https://demo.experitest.com/wd/hub"), desiredCapabilities);
+				user= new UserActions(driver);
+	    }
+
+	    driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+  
+}
+  @BeforeClass
+  public void login() throws IOException {
+	user= new UserActions(driver);
+	
+	System.out.println("Before Class");
+	System.out.println(accessKey);
+	if(accessKey==null)
+	{
+		System.out.println("Emulator ENter / Device");
+		user.Click(pageObjects_Swetha.loginwithgoogle);
+		user.Click(pageObjects_Swetha.displayname);
+	}
+    
+  }
+  @Test(dataProvider = "dp")
+  public void sampleTest(String recepieName) throws InterruptedException {
+
+         user.Click(pageObjects_Swetha.SearchBox); 
+         user.Click(pageObjects_Swetha.clicksearch);
+		 user.ClearAndSendKeys(pageObjects_Swetha.EnterSeach, recepieName);
+		 user.Click(pageObjects_Swetha.Suggestion); 
+		 user.Click(pageObjects_Swetha.ImageView); 
+		 user.Click(pageObjects_Swetha.AddtoReceipes);
+		 user.Click(pageObjects_Swetha.MyReceipes);
+		 user.Click(pageObjects_Swetha.Discover);
+		 user.Click(pageObjects_Swetha.BackButton);
+		  
+  }
+
+// @Test(dependsOnMethods ="sampleTest")
+// public void sampleTest1(String recepieName) {
+//	 
+//	
+//	 user.Click(pageObjects_Swetha.clicksearch);
+// 
+//	
+// 
+// }
+ @Ignore
+  @DataProvider
+  public Object[][] dp1() throws SQLException {
+	  return ExcelUtil.getTestData("./testData.xlsx", "Swetha")  ;
+  }
+  
+ 
+
+  @DataProvider
+  public Object[][] dp() throws SQLException {
+	 //return new Object [][] {{"Fried rice"},{"Pasta"}};
+		//return ExcelUtil.getTestData("./testData.xlsx", "Swetha")  ;
+	  
+	Object data[][]= DatabaseConnection.getDataFromDatabase("Swetha");     
+          return data;
+      }
+	  
+  
+  
+
+  @AfterClass
+  public void tearDown() {
+	  
+    driver.quit();
+  }
+}
